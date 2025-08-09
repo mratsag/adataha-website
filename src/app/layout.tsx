fc2e3635d97d1713f53cb4d542d2a6d13b8a00be
Murat <mrat.sag@hotmail.com>
2025-08-09 21:47:49 +0300

    Add Google Analytics tracking with ID G-6NH2X5GNGG

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -91,10 +91,10 @@ export default function RootLayout({
         </ThemeProvider>
         
         {/* Google Analytics */}
-        {process.env.NEXT_PUBLIC_GA_ID && (
+        {"G-6NH2X5GNGG" && (
           <>
             <Script
-              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+              src="https://www.googletagmanager.com/gtag/js?id=G-6NH2X5GNGG"
               strategy="afterInteractive"
             />
             <Script id="google-analytics" strategy="afterInteractive">
@@ -102,7 +102,7 @@ export default function RootLayout({
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-                gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+                gtag('config', 'G-6NH2X5GNGG');
               `}
             </Script>
           </>
